9a9f299ba7452c2c8f41652694654159fc553611
John Cooper <jfrancec@unm.edu>
2021-04-04 14:12:39 -0600

    MazeTile and Maze initial commit

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -1,18 +1,23 @@
 package maze;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Maze <Tile extends MazeTile> {
-    List<List<Tile>> maze;
-    Tile startTile;
-    Tile endTile;
+public class Maze {
+    private List<List<MazeTile>> maze;
+    private MazeTile startTile;
+    private MazeTile endTile;
+    private int width;
 
     /**
      * @param width The width of the board
      * @param fillWalls If all of the walls in the board should be filled
      */
     public Maze(int width, boolean fillWalls) {
+        maze = new ArrayList<>();
+        for(int i = 0; i < width; i++) {
 
+        }
     }
 
     // Things with startTile and endTile
--- src/maze/MazeTile.java
+++ src/maze/MazeTile.java
@@ -1,24 +1,57 @@
 package maze;
 
 public class MazeTile {
+    private boolean northWall;
+    private boolean southWall;
+    private boolean westWall;
+    private boolean eastWall;
+    private int index;
 
     public MazeTile() {
+        northWall = true;
+        eastWall = true;
+        southWall = true;
+        westWall = true;
+        index = 0;
+    }
 
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
     }
 
     public boolean hasNorthWall() {
-        return true;
+        return northWall;
     }
 
     public boolean hasEastWall() {
-        return true;
+        return eastWall;
     }
 
     public boolean hasSouthWall() {
-        return true;
+        return southWall;
     }
 
     public boolean hasWestWall() {
-        return true;
+        return westWall;
+    }
+
+    public void setEastWall(boolean eastWall) {
+        this.eastWall = eastWall;
+    }
+
+    public void setNorthWall(boolean northWall) {
+        this.northWall = northWall;
+    }
+
+    public void setSouthWall(boolean southWall) {
+        this.southWall = southWall;
+    }
+
+    public void setWestWall(boolean westWall) {
+        this.westWall = westWall;
     }
 }
