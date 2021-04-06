5c647337bf500f1cf3dee2771b96dd7d418da1c0
John Cooper <jfrancec@unm.edu>
2021-04-05 18:26:27 -0600

    Maze and MazeTile comments

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -1,12 +1,24 @@
+/**
+ * CS351 Project 4 - Mazes
+ * Authors : John Cooper && Isha Chauhan
+ *
+ * The data structure holding all of the relevant information about
+ * the maze
+ */
+
 package maze;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Maze {
+    // The 2D structure with the tiles in the maze
     private List<List<MazeTile>> maze;
+    // The start tile of the maze
     private MazeTile startTile;
+    // The end tile of the maze
     private MazeTile endTile;
+    // The width of the maze
     private int width;
 
     /**
@@ -24,60 +36,115 @@ public class Maze {
         }
     }
 
-    // Things with startTile and endTile
-
-    // Add wall methods, like the remove wall ones
-
+    /**
+     * @param row The row of the desired tile
+     * @param col The column of the desired tile
+     * @return The tile at (row, col) in the maze
+     */
     public MazeTile getTile(int row,int col){
         List<MazeTile> rowTile = maze.get(row);
         MazeTile colTile = rowTile.get(col);
         return colTile;
     }
 
+    /**
+     * Removes the wall to the north of the tile listed
+     * @param row The desired row
+     * @param col The desired column
+     */
     public void removeWallNorthOf(int row, int col) {
         getTile(row,col).setNorthWall(false);
     }
 
+    /**
+     * Removes the wall to the east of the tile listed
+     * @param row The desired row
+     * @param col The desired column
+     */
     public void removeWallEastOf(int row, int col) {
         getTile(row,col).setEastWall(false);
     }
 
+    /**
+     * Removes the wall to the south of the tile listed
+     * @param row The desired row
+     * @param col The desired column
+     */
     public void removeWallSouthOf(int row, int col) {
         getTile(row,col).setSouthWall(false);
     }
 
+    /**
+     * Removes the wall to the west of the tile listed
+     * @param row The desired row
+     * @param col The desired column
+     */
     public void removeWallWestOf(int row, int col) {
         getTile(row,col).setWestWall(false);
     }
 
+    /**
+     * Adds the wall to the north of the tile listed
+     * @param row The desired row
+     * @param col The desired column
+     */
     public void addWallNorthOf(int row, int col){
         getTile(row,col).setNorthWall(true);
     }
 
+    /**
+     * Adds the wall to the east of the tile listed
+     * @param row The desired row
+     * @param col The desired column
+     */
     public void addWallEastOf(int row, int col){
         getTile(row,col).setEastWall(true);
     }
 
+    /**
+     * Adds the wall to the south of the tile listed
+     * @param row The desired row
+     * @param col The desired column
+     */
     public void addWallWestOf(int row, int col){
         getTile(row,col).setWestWall(true);
     }
 
+    /**
+     * Adds the wall to the west of the tile listed
+     * @param row The desired row
+     * @param col The desired column
+     */
     public void addWallSouthOf(int row, int col){
         getTile(row,col).setSouthWall(true);
     }
 
+    /**
+     * Sets the start tile
+     */
     public void setStartTile(){
         this.startTile = startTile;
     }
 
+    /**
+     * Sets the end tile
+     */
     public void setEndTile(){
         this.endTile = endTile;
     }
 
+    /**
+     * Gives the start tile
+     * @return The start tile of the maze
+     */
     public MazeTile getStartTile() {
         return startTile;
     }
 
+    /**
+     * Gives the end tile
+     * @return The end tile of the maze
+     */
     public MazeTile getEndTile() {
         return endTile;
     }
--- src/maze/MazeTile.java
+++ src/maze/MazeTile.java
@@ -1,12 +1,27 @@
+/**
+ * CS351 Project 4 - Mazes
+ * Authors : John Cooper && Isha Chauhan
+ *
+ * The individual cells of the maze
+ */
+
 package maze;
 
 public class MazeTile {
+    // true if the wall to the north of the tile is present
     private boolean northWall;
+    // true if the wall to the south of the tile is present
     private boolean southWall;
+    // true if the wall to the west of the tile is present
     private boolean westWall;
+    // true if the wall to the east of the tile is present
     private boolean eastWall;
+    // A useful index/value in creating
     private int index;
 
+    /**
+     * Creates a maze tile
+     */
     public MazeTile() {
         northWall = true;
         eastWall = true;
@@ -15,42 +30,76 @@ public class MazeTile {
         index = 0;
     }
 
+    /**
+     * @return The index of the tile
+     */
     public int getIndex() {
         return index;
     }
 
+    /**
+     * @param index The index/relevant value of the tile
+     */
     public void setIndex(int index) {
         this.index = index;
     }
 
+    /**
+     * @return true if the tile has a north wall
+     */
     public boolean hasNorthWall() {
         return northWall;
     }
 
+    /**
+     * @return true if the tile has a east wall
+     */
     public boolean hasEastWall() {
         return eastWall;
     }
 
+    /**
+     * @return true if the tile has a south wall
+     */
     public boolean hasSouthWall() {
         return southWall;
     }
 
+    /**
+     * @return true if the tile has a west wall
+     */
     public boolean hasWestWall() {
         return westWall;
     }
 
+    /**
+     * Sets the value of the east wall of the given tile
+     * @param eastWall The new value for if the east wall is present
+     */
     public void setEastWall(boolean eastWall) {
         this.eastWall = eastWall;
     }
 
+    /**
+     * Sets the value of the north wall of the given tile
+     * @param northWall The new value for if the north wall is present
+     */
     public void setNorthWall(boolean northWall) {
         this.northWall = northWall;
     }
 
+    /**
+     * Sets the value of the south wall of the given tile
+     * @param southWall The new value for if the south wall is present
+     */
     public void setSouthWall(boolean southWall) {
         this.southWall = southWall;
     }
 
+    /**
+     * Sets the value of the west wall of the given tile
+     * @param westWall The new value for if the west wall is present
+     */
     public void setWestWall(boolean westWall) {
         this.westWall = westWall;
     }
